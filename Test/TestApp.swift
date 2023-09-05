613f9ff07004ec7302c687408f759fe28c7ecb5a
psusman <pasusman@gmail.com>
2023-09-05 15:42:55 -0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Phillip Susman on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
